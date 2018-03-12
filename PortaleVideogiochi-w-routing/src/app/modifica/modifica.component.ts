c6169df636b2320fcf47ebf4d947740c0ed972cc
fiacca98 <lui.gi98@live.it>
2018-03-12 12:08:05 +0100

    ReactiveForm completato

--- PortaleVideogiochi-w-routing/src/app/modifica/modifica.component.ts
+++ PortaleVideogiochi-w-routing/src/app/modifica/modifica.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GameItem } from '../../GameItem';
 import { GameListService } from '../game-list.service';
 import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modifica',
@@ -14,12 +15,29 @@ export class ModificaComponent implements OnInit {
   copyGame: GameItem;
   currentGame: GameItem;
 
-  constructor(private route: ActivatedRoute ,private gameListService: GameListService) { 
+  gameForm: FormGroup;
+
+  constructor(private route: ActivatedRoute ,private gameListService: GameListService, private formBuilder: FormBuilder) { 
+    
     this.route.params.subscribe(params => {
       if(params["id"] != "" && params["id"] != null && params["id"] != "x"){
         this.copyGame = this.gameListService.getGameById(params["id"]);
         this.currentGame = gameListService.clone(this.copyGame);
+        this.createForm();
       }
+    });
+    
+  }
+
+  createForm() {
+    this.gameForm = this.formBuilder.group({
+      nome: [this.currentGame.nome,Validators.required],
+      descrizione: [this.currentGame.descrizione,Validators.required],
+      genere: [this.currentGame.genere,Validators.required],
+      rating: [this.currentGame.rating,Validators.required],
+      prezzo: [this.currentGame.prezzo,Validators.required],
+      annoUscita: [this.currentGame.annoUscita,Validators.required],
+      consoleType: [this.currentGame.consoleType,Validators.required],      
     });
   }
 
@@ -29,6 +47,7 @@ export class ModificaComponent implements OnInit {
 
       this.copyGame = this.gameListService.getGameByName(this.nomeGioco);
       this.currentGame = this.gameListService.clone(this.copyGame);
+      this.createForm();
     }
     else{
       alert("Gioco non presente");
@@ -36,6 +55,13 @@ export class ModificaComponent implements OnInit {
     
 }
   saveData(){
+    this.currentGame.nome = this.gameForm.get("nome").value;
+    this.currentGame.descrizione = this.gameForm.get("descrizione").value;
+    this.currentGame.genere = this.gameForm.get("genere").value;
+    this.currentGame.rating = this.gameForm.get("rating").value;
+    this.currentGame.prezzo = this.gameForm.get("prezzo").value;
+    this.currentGame.annoUscita = this.gameForm.get("annoUscita").value;
+    this.currentGame.consoleType = this.gameForm.get("consoleType").value;
     this.gameListService.setGame(this.currentGame);
   }
 
@@ -43,6 +69,15 @@ export class ModificaComponent implements OnInit {
   }
 
   ngFormReset(){
+    this.gameForm.setValue({
+      nome: this.currentGame.nome,
+      descrizione: this.currentGame.descrizione,
+      genere: this.currentGame.genere,
+      rating: this.currentGame.rating,
+      prezzo: this.currentGame.prezzo,
+      annoUscita: this.currentGame.annoUscita,
+      consoleType: this.currentGame.consoleType    
+    });
     this.currentGame = this.gameListService.clone(this.copyGame);
   }
 
